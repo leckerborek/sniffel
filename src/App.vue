59e98cd393be0e7cb0c32372985a837c25903899
jlorek <lorek.jens@gmail.com>
2020-03-28 19:06:46 +0100

    ???

--- src/App.vue
+++ src/App.vue
@@ -28,32 +28,29 @@
 
     <v-content>
       <v-container class="fill-height" fluid>
-        <v-row align="center" justify="center">
+        <!-- <v-row align="center" justify="center">
           <v-col class="text-center">
-            <v-tooltip left>
-              <template v-slot:activator="{ on }">
-                <v-btn :href="source" icon large target="_blank" v-on="on">
-                  <v-icon large>mdi-code-tags</v-icon>
-                </v-btn>
-              </template>
-              <span>Source</span>
-            </v-tooltip>
-
-            <v-tooltip right>
-              <template v-slot:activator="{ on }">
-                <v-btn
-                  icon
-                  large
-                  href="https://codepen.io/johnjleider/pen/zgxeLQ"
-                  target="_blank"
-                  v-on="on"
-                >
-                  <v-icon large>mdi-codepen</v-icon>
-                </v-btn>
-              </template>
-              <span>Codepen</span>
-            </v-tooltip>
           </v-col>
+        </v-row>-->
+        <v-row align="center">
+          <vue-webrtc
+            ref="webrtc"
+            width="100%"
+            :roomId="roomId"
+            v-on:joined-room="logEvent"
+            v-on:left-room="logEvent"
+            v-on:open-room="logEvent"
+            v-on:share-started="logEvent"
+            v-on:share-stopped="logEvent"
+            @error="onError"
+          ></vue-webrtc>
+        </v-row>
+
+        <v-row align="center">
+          <v-btn @click="onJoin">Join</v-btn>
+          <v-btn @click="onLeave">Leave</v-btn>
+          <v-btn @click="onCapture">Capture Photo</v-btn>
+          <v-btn @click="onShareScreen">Share Screen</v-btn>
         </v-row>
       </v-container>
     </v-content>
@@ -69,7 +66,29 @@ export default {
     source: String
   },
   data: () => ({
-    drawer: null
-  })
+    drawer: null,
+    img: null,
+    roomId: "public-room"
+  }),
+  methods: {
+    onCapture() {
+      this.img = this.$refs.webrtc.capture();
+    },
+    onJoin() {
+      this.$refs.webrtc.join();
+    },
+    onLeave() {
+      this.$refs.webrtc.leave();
+    },
+    onShareScreen() {
+      this.img = this.$refs.webrtc.shareScreen();
+    },
+    onError(error, stream) {
+      console.log("On Error Event", error, stream);
+    },
+    logEvent(event) {
+      console.log("Event : ", event);
+    }
+  }
 };
 </script>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
 import vuetify from './plugins/vuetify';
+import { WebRTC } from 'vue-webrtc';
 
 Vue.config.productionTip = false
+Vue.component(WebRTC.name, WebRTC);
 
 new Vue({
   vuetify,
